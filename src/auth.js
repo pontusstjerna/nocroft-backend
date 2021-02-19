7f421560055511b519aa36c80288bf0fed821f51
Pontus Stjernström <pontus@stjernstrom.nu>
2021-02-19 11:35:35 +0100

    Prepare for mqtt and removal of servers in rasperry pis

--- src/auth.js
+++ src/auth.js
@@ -1,61 +1,53 @@
-import { verifyJWT, createJWT, verifyJWT_MW} from './jwt.js';
-import btoa from 'btoa';
+import { verifyJWT, createJWT, verifyJWT_MW } from "./jwt.js"
+import btoa from "btoa"
 
-const authorizeUserPassword = userPassword => {
-    const maybeUser = process.env.USERS.split(';').find(config =>  userPassword === btoa(config));
+const authorizeUserPassword = (userPassword) => {
+  const maybeUser = process.env.USERS.split(";").find(
+    (config) => userPassword === btoa(config)
+  )
 
-    if (!maybeUser) {
-        return false;
-    }
+  if (!maybeUser) {
+    return false
+  }
 
-    // Return the users username
-    return maybeUser.split(':')[0];
+  // Return the users username
+  return maybeUser.split(":")[0]
 }
 
-export const login = ((req, res) => {
-    const user = authorizeUserPassword(req.body);
+export const login = (req, res) => {
+  const user = authorizeUserPassword(req.body)
 
-    if (!user) {
-        res.status(401).send("Invalid username or password.");
-        return;
-    }
+  if (!user) {
+    res.status(401).send("Invalid username or password.")
+    return
+  }
 
-    const token = createJWT({
-        sessionData: user,
-        maxAge: 600,
-    });
+  const token = createJWT({
+    sessionData: user,
+    maxAge: 600,
+  })
 
-    res.status(200).send(token);
-});
+  res.status(200).send(token)
+}
 
-export const checkSocketAuthorized = req => {
-    const authHeader = req.headers.authorization;
+export const ioVerifyJWT_MW = (socket, next) =>
+  verifyJWT(socket.handshake.auth.token)
+    .then((decoded) => {
+      socket.decoded = decoded
+      next()
+    })
+    .catch((error) => next(error))
 
-    return new Promise(resolve => {
-        if (authHeader) {
-            const splitHeader = authHeader.split(' ');
+export const checkLogin = (req, res) => {
+  res
+    .status(200)
+    .send(
+      `Welcome ${req.user}, I have missed you. This is the backend calling. :)`
+    )
+}
 
-            if (splitHeader[0] === 'bearer') {
-                const token = splitHeader[1];
-
-                verifyJWT(token).then(decodedToken => {
-                    req.user = decodedToken.data;
-                }).catch(err => {
-                    resolve({authorized: false, code: 401, status: "Unauthorized: " + err.message});
-                }).then(() => resolve({authorized: true, code: 200, status: 'OK'}));
-            } else {
-                resolve({authorized: false, code: 400, status: 'Invalid authorization header.'});
-            }
-        } else {
-            resolve({authorized: false, code: 400, status: 'Unauthorized (no authorization header).'});
-        }
-    });
-};
-
-export const checkLogin = ((req, res) => {
-    res.status(200).send(`Welcome ${req.user}, I have missed you. This is the backend calling. :)`);
-});
-
-export const getAccessToken = ((req, res) => {
-    verifyJWT_MW(req, res, () => res.status(200).send(createJWT({maxAge: 2, sessionData: req.user})));
-});
+export const getAccessToken = (req, res) => {
+  verifyJWT_MW(req, res, () =>
+    res.status(200).send(createJWT({ maxAge: 2, sessionData: req.user }))
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,84 +1,114 @@
-import express from 'express';
-import http from 'http';
-import url from 'url';
-import httpProxy from 'http-proxy';
-import bodyParser from 'body-parser';
-import { login, checkLogin, getAccessToken, checkSocketAuthorized } from './auth.js'
-import video from './video.js';
-import { config } from 'dotenv';
-import { verifyJWT_MW, verifyJWT } from './jwt.js';
+import express from "express"
+import http from "http"
+import bodyParser from "body-parser"
+import { login, checkLogin, getAccessToken, ioVerifyJWT_MW } from "./auth.js"
+import video from "./video.js"
+import { config } from "dotenv"
+import { verifyJWT_MW } from "./jwt.js"
+import mqtt from "mqtt"
+import startSocket from "socket.io"
 
-config();
+config()
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 8080
 
-const proxy = httpProxy.createProxyServer({
-    target: process.env.PROXY_TARGET,
-    ws: true
-});
-const server = http.createServer(app);
+const server = http.createServer(app)
+const io = startSocket(server)
 
-app.use(bodyParser.text());
+const mqttClient = mqtt.connect({
+  hostname: process.env.MQTT_BROKER_URL || "mqtt://127.0.0.1",
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  protocol: "mqtt",
+})
 
-app.post('/login', login);
-app.get('/access-token', verifyJWT_MW, getAccessToken);
-app.get('/login', verifyJWT_MW, checkLogin);
-process.env.VIDEO_ENDPOINTS.split(';').forEach(endpoint => app.get(endpoint, verifyJWT_MW, video));
+mqttClient.on("connect", () => {
+  console.log("mqtt client connected successfully")
+  // TODO: Possibly allow for client to specify which robot to control
+  mqttClient.subscribe(["robotpi/started", "robotpi/status"], error => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log("Subscribed to topics with mqtt")
+    }
+  })
+
+  mqttClient.on("message", (topic, message) => {
+    console.log({ topic, message: message.toString() })
+
+    switch (topic) {
+      case "robotpi/started":
+        io.emit("started", message.toString())
+        break
+      case "robotpi/status":
+        io.emit("status", message.toString())
+        break
+    }
+  })
+})
+
+mqttClient.on("error", error => console.log(error))
+
+app.use(bodyParser.text())
+
+app.post("/login", login)
+app.get("/access-token", verifyJWT_MW, getAccessToken)
+app.get("/login", verifyJWT_MW, checkLogin)
+process.env.VIDEO_ENDPOINTS.split(";").forEach(endpoint =>
+  app.get(endpoint, verifyJWT_MW, video)
+)
+
+io.use(ioVerifyJWT_MW).on("connection", socket => {
+  socket.on("started", () => mqttClient.publish("robotpi", "started"))
+  socket.on("status", () => mqttClient.publish("robotpi", "status"))
+})
 
 // SOCKET.IO
-app.use('/socket.io', (req, res) => {
-    checkSocketAuthorized(req).then(({authorized, code, status}) => {
-        if (authorized) {
-            console.log('Socket authorized');
-            proxy.web(req, res, {target: process.env.PROXY_TARGET + '/socket.io'});
-        } else {
-            console.log('Socket request failed: ' + code + ": " + status);
-            res.status(code).send(status);
-        }
-    })
-});
-
-proxy.on('proxyReqWs', (proxyReqWs, req, res) => {
-    //console.log('Got socket request in proxy from ' + req.headers.host);
-})
-
-proxy.on('proxyReq', (proxyReq, req, res) => {
-    //console.log('Got request in proxy from ' + req.headers.host);
-})
-
-proxy.on('error', err => {
-    //console.log(`ERROR with socket: ${err.code}`);
-})
+/*app.use("/socket.io", (req, res) => {
+  checkSocketAuthorized(req).then(({ authorized, code, status }) => {
+    if (authorized) {
+      // TODO: Maybe use unique IDs for different robots in the future?
+      client.publish("robotpi", )
+    } else {
+      console.log("Socket request failed: " + code + ": " + status)
+      res.status(code).send(status)
+    }
+  })
+})*/
 
 // VIDEO
-server.on('upgrade', (req, socket, header) => {
-    const videoEndpoints = process.env.VIDEO_ENDPOINTS.split(';');
-    if (videoEndpoints.some(e => req.url.startsWith(e))) {
-        const queryParameters = url.parse(req.url, true).query;
-        verifyJWT(queryParameters.access_token)
-            .then(() => {
-                let endpoint;
-                let proxyTarget;
+/*
+server.on("upgrade", (req, socket, header) => {
+  const videoEndpoints = process.env.VIDEO_ENDPOINTS.split(";")
+  if (videoEndpoints.some((e) => req.url.startsWith(e))) {
+    const queryParameters = url.parse(req.url, true).query
+    verifyJWT(queryParameters.access_token)
+      .then(() => {
+        let endpoint
+        let proxyTarget
 
-                for (let i = 0; i < videoEndpoints.length; i++) {
-                    if (req.url.startsWith(videoEndpoints[i])) {
-                        endpoint = videoEndpoints[i];
-                        proxyTarget = process.env.PROXY_VIDEO_TARGETS.split(';')[i];
-                    }
-                }
+        for (let i = 0; i < videoEndpoints.length; i++) {
+          if (req.url.startsWith(videoEndpoints[i])) {
+            endpoint = videoEndpoints[i]
+            proxyTarget = process.env.PROXY_VIDEO_TARGETS.split(";")[i]
+          }
+        }
 
-                console.log(`Video endpoint "${endpoint}" was authorized, proxying to ${proxyTarget}`);
-                proxy.ws(req, socket, header, {target: proxyTarget})
-            })
-            .catch(e =>{
-                console.log(e.message);
-                console.log('Unauthorized video socket listener!');
-                socket.destroy();
-            });
-    } else {
-        proxy.ws(req, socket, header);
-    }
-});
+        console.log(
+          `Video endpoint "${endpoint}" was authorized, proxying to ${proxyTarget}`
+        )
+        proxy.ws(req, socket, header, { target: proxyTarget })
+      })
+      .catch((e) => {
+        console.log(e.message)
+        console.log("Unauthorized video socket listener!")
+        socket.destroy()
+      })
+  } else {
+    proxy.ws(req, socket, header)
+  }
+})
+*/
 
 server.listen(port, () => console.log(`Server listening on ${port}.`))
